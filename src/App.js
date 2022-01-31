14474e8d4cbdbff7c0bdf2450b5c74236fb223a0
laidanob <41763636+laidanob@users.noreply.github.com>
2022-01-30 21:42:20 -0300

    llamado de Cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter , Routes, Route,Navigate} from "react-router-dom"
 import { ItemDetailContainer } from "./components/ItemDetailContainer/ItemDetailContainer";
 import "./estilo.scss";
 import { Contacto } from "./components/Contacto/Contacto";
+import { Cart } from "./components/Cart/Cart";
 
 function App() {
   return (
@@ -22,7 +23,6 @@ function App() {
       <Route path="/contacto" element={<Contacto/>} />
       <Route path="/cart" element={<Cart/>} />
       <Route path="*" element={<Navigate to="/"/>}/>
-
       </Routes>
       </BrowserRouter> 
     </div>
